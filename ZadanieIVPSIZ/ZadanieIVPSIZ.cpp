d78763ae2bfebd876feafe6fe5645b61d7cb788d
hamstler <Knaskreski@gmail.com>
2022-04-11 22:27:21 +0200

    Add lines which creates files

--- ZadanieIVPSIZ/ZadanieIVPSIZ.cpp
+++ ZadanieIVPSIZ/ZadanieIVPSIZ.cpp
@@ -19,7 +19,11 @@ void createFile(const string name, const int count, const char value) {
 int main()
 {
     cout << "Hello World!\n";
-    //createFile("400MB.bin", 419430400, 0x55);
+    //createFile("100bytes_1.bin", 100, 0x55);
+    //createFile("100bytes_2.bin", 100, 0x55);
+    //createFile("100bytes_3_changed_by_10_bytes.bin", 100, 0x55);
+    //createFile("400MB_1.bin", 419430400, 0x55);
+    //createFile("400MB_2.bin", 419430400, 0x50);
     cout << "wykonano create file";
 }
 
