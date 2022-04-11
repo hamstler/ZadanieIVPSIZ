2e603f7f98aff7d97578ffe525e372f9f6d4109a
hamstler <Knaskreski@gmail.com>
2022-04-11 21:52:08 +0200

    Dodaj pliki projektów.

---
+++ ZadanieIVPSIZ/ZadanieIVPSIZ.cpp
@@ -0,0 +1,25 @@
+﻿// TaskIV.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void createFile(const string name, const int count, const char value) {
+    int i = 0;
+    ofstream MyFile(name);
+
+    for (i = 0; i < count; i++) {
+        MyFile << value;
+
+    }
+
+};
+int main()
+{
+    cout << "Hello World!\n";
+    //createFile("400MB.bin", 419430400, 0x55);
+    cout << "wykonano create file";
+}
+
