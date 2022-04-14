82cfa900b890fb6e00262b6e67e78b28220848bc
hamstler <Knaskreski@gmail.com>
2022-04-14 18:10:57 +0200

    added functions and comments

--- ZadanieIVPSIZ/ZadanieIVPSIZ.cpp
+++ ZadanieIVPSIZ/ZadanieIVPSIZ.cpp
@@ -3,17 +3,23 @@
 
 #include <iostream>
 #include <fstream>
-#include "FileAdd.h"
+#include "Functions.h"
 
-using namespace std;
+uint8_t hammingDistance(uint8_t n1, uint8_t n2);
 
-
-
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello World!\n";
+    std::cout << "BER Checker!\n";
+    std::cout << "argc = : " << argc; "\n";
+
+    int iter = 0;
+    for (iter = 0; iter < argc; iter++) {
+        std::cout << "argv[" << iter << "] =" << argv[iter]; "\n";
+    }
+
+    CompareFunction(argv[1], argv[2]);
     //createFile("100bytes_1.bin", 100, 0x55);
-    createFile("100bytes_2.bin", 100, 0x55);
+    //createFile("100bytes_2.bin", 100, 0x55);
     //createFileRandom("100bytes_3_changed_by_10_bytes.bin", 100, 0x55, 0x45);
     //createFile("400MB_1.bin", 419430400, 0x55);
     //createFile("400MB_2.bin", 419430400, 0x50);
